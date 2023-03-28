a680f009ac0e411791a71f83ce30e9a2a8c5a495
Robotto <mark@moore.dk>
2023-03-28 22:57:45 +0200

    Still not running, but getting closer...

--- Plasma.ino
+++ Plasma.ino
@@ -3,61 +3,106 @@ int P2=D3;
 int P3=D4;
 int R=D5;
 
+int A1=D7;
+int A2=D8;
+
+
+#define ANODE_ON LOW
+#define ANODE_OFF HIGH
+#define CATHODE_ON HIGH
+#define CATHODE_OFF LOW
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 //all pins to output
-pinMode(P1,OUTPUT);
-pinMode(P2,OUTPUT);
-pinMode(P3,OUTPUT);
+pinMode(P1, OUTPUT);
+pinMode(P2, OUTPUT);
+pinMode(P3, OUTPUT);
 pinMode(R, OUTPUT);
+pinMode(A1, OUTPUT);
+pinMode(A2, OUTPUT);
 
-//all pins to low
-digitalWrite(P1,LOW);
-digitalWrite(P2,LOW);
-digitalWrite(P3,LOW);
-digitalWrite(R, LOW);
+//all phase pins to low turns on
+digitalWrite(P1,CATHODE_OFF);
+digitalWrite(P2,CATHODE_OFF);
+digitalWrite(P3,CATHODE_OFF);
+digitalWrite(R, CATHODE_OFF);
 
+//anodes on!
+digitalWrite(A1, ANODE_ON);
+digitalWrite(A2, ANODE_ON);
 }
 
+#define PHASOR_DELAY_MICROSECONDS 70
+#define RESET_DELAY_MICROSECONDS 140
+
+uint8_t segmentCounter = 0; //continuously runing from 0 to 100.
+unsigned long nextPhaseAtMicros = 0;
+
+uint8_t target_1=0;
+uint8_t target_2=0;
+
+unsigned long nextRetargeting=0;
+int delta=1;
 void loop() {
-  // put your main code here, to run repeatedly:
-  //phasor runs at 70 hz = Every 14 miliseconds (counts to 100 in 1.40 seconds)
+// put your main code here, to run repeatedly:
 
-  delayMicroseconds(140);
-  phasor();  
+if(millis()>nextRetargeting){
+  if(target_1>99) delta=-1;
+  if(target_1==0) delta=1;
+  target_1+=delta;
+  target_2=target_1;
+  nextRetargeting=millis()+5;
+  }
+if ( micros() > nextPhaseAtMicros)  
+  {
+    phasor();  //phasor runs at 70 hz = Every 14 miliseconds (counts to 100 in 1.40 seconds)
+    //segmentCounter=segmentCounter%100; //keep going  from 0 to 99 (1-100)
+    if(segmentCounter==100){
+      segmentCounter=0;
 
+      //All phases to 0:
+      digitalWrite(P1,CATHODE_OFF);
+      digitalWrite(P2,CATHODE_OFF);
+      digitalWrite(P3,CATHODE_OFF);
+
+      //Pulse the reset pin:
+      digitalWrite(R, CATHODE_ON);
+      delayMicroseconds(RESET_DELAY_MICROSECONDS);
+      digitalWrite(R, CATHODE_OFF);
+    }
+    nextPhaseAtMicros=micros()+PHASOR_DELAY_MICROSECONDS;
+  }  
 }
-
-
+ 
 void phasor()
 {
-  static uint8_t segmentCounter=0;
-
-segmentCounter%100; //keep it going from 0-99
-
-//reset pulse duration: 140uS
-  if(segmentCounter==0){
-    digitalWrite(R,HIGH);
-    delayMicroseconds(140);
-    digitalWrite(R,LOW);
-  }
-
-  switch (segmentCounter%3){ //Every third count comes back to the same state.
+//Every third count comes back to the same state.
+  switch ( segmentCounter%3 ){ 
     case 0:
-      digitalWrite(P1,HIGH);
-      digitalWrite(P3,LOW);
+      digitalWrite(P1, CATHODE_ON);
+      digitalWrite(P2, CATHODE_OFF);
+      digitalWrite(P3, CATHODE_OFF);
       break;
     case 1:
-      digitalWrite(P1,LOW);
-      digitalWrite(P2,HIGH);   
+      digitalWrite(P1, CATHODE_OFF);
+      digitalWrite(P2, CATHODE_ON);
+      digitalWrite(P3, CATHODE_OFF);
       break;
     case 2:
-      digitalWrite(P2,LOW);
-      digitalWrite(P3,HIGH);
+      digitalWrite(P1, CATHODE_OFF);
+      digitalWrite(P2, CATHODE_OFF);
+      digitalWrite(P3, CATHODE_ON);
       break;
-    default:
+    default: //should never happen, but here we go anyway... 
       break;
   }
-  delayMicroseconds(70);
+  
+  //Pulse the Anodes:
+  digitalWrite(A1,ANODE_ON);
+  if( target_1 > segmentCounter) digitalWrite(A1,ANODE_OFF);
+  digitalWrite(A2,ANODE_ON);
+  if( target_2 > segmentCounter) digitalWrite(A2,ANODE_OFF);
+
   segmentCounter++;
 }
